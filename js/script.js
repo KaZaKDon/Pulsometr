f1ffc02713aa9796c0086eb2ed68171a3516c29c
Dmitriy <strannik19742@mail.ru>
2024-12-08 17:18:12 +0300

    исправлена модалка

--- js/script.js
+++ js/script.js
@@ -1,97 +1,110 @@
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+    speed: 1200,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="icon/chevron left solid.png"></img></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="icon/chevron right solid.png"></img></button>',
+  });
 
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        slidesToShow: 1,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icon/chevron left solid.png"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icon/chevron right solid.png"></img></button>'
-    });
-
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-    });
-
-    function toggleSlide(item) {
-        $(item) .each(function(i) {
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            });
-        });
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
     }
+  );
 
-    toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back');
-
-    $('[data-modal=consultation]').on('click', function() {
-        $('.overlay, #consultation').fadeIn('slow');
-    });
-    $('.modal__close').on('click', function() {
-        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-    });
-
-    $('.button_mini').each(function(i) {
-        $(this).on('click', function() {
-            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-            $('.overlay, #order').fadeIn('slow');
-        });
-    });
-
-    function valideForms(form) {
-        $(form).validate({
-            rules: {
-                name: "required",
-                phone: "required",
-                email: {
-                    required: true,
-                    email: true
-                }
-            },
-            messages: {
-                name: "Введите свое имя",
-                phone: "Введите коректрый номер телефона",
-                email: {
-                required: "Введите свой почтовый адрес",
-                email: "Введите коректный почтовый адрес name@domain.com"
-                }
-            }
-        });
-    };
-
-    valideForms('#consultation-form');
-    valideForms('#consultation form');
-    valideForms('#order form');
-
-    $('input[name=phone]').mask("+7 (999) 999-99-99");
-
-    $('form').submit(function(e) {
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
         e.preventDefault();
-
-        if(!$(this).valid()) {
-            return;
-        };
-        $.ajax({
-            type: "POST",
-            url: "mailer/smart.php",
-            data: $(this).serialize()
-        }).done(function() {
-            $(this).find("input").val("");
-
-
-            $('form').trigger('reset')
-        });
-        return false;
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+      });
     });
+  }
 
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 1600) {
-            $('.pageup').fadeIn();
-        } else {
-            $('.pageup').fadeOut();
-        }
-    })
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
+
+  //Modal
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+  });
+
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
+
+  function valideForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: "Введите свое имя",
+        phone: "Введите коректрый номер телефона",
+        email: {
+          required: "Введите свой почтовый адрес",
+          email: "Введите коректный почтовый адрес name@domain.com",
+        },
+      },
+    });
+  }
+
+  valideForms("#consultation-form");
+  valideForms("#consultation form");
+  valideForms("#order form");
+
+  $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+  $("form").submit(function (e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+      return;
+    }
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+
+      $("form").trigger("reset");
+    });
+    return false;
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $(".pageup").fadeIn();
+    } else {
+      $(".pageup").fadeOut();
+    }
+  });
 });
